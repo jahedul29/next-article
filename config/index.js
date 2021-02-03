ba7bcf4b703e1ec4151406788bd95194c45bfc60
[jahedul29] <[jahedulh1@gmail.com]>
2021-02-04 00:16:10 +0600

    changing config file

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://next-article-3z7unludk.vercel.app";
+  : "https://next-article-two.vercel.app";
